de735fc1f80f8ae3b15fc2ec164d22257fcd9611
David <DKnight96@sbcglobal.net>
2017-07-03 22:31:24 -0700

    Added tremolo and digital filtering functions to c55 code, also fixed the flange effect to actually work

--- Software/C5535 ezdsp/effects.h
+++ Software/C5535 ezdsp/effects.h
@@ -9,6 +9,7 @@
 #define EFFECTS_H_
 
 #include <math.h>
+#include "FIR_filters_asm.h"
 
 #define circ_buffer_size 39500 //memory corrupted at 39630-39830
 #define SAMPLES_PER_SECOND 48000
@@ -18,5 +19,7 @@ void delay_sample(int);
 int delay(int, unsigned long);
 int flange(int, float, unsigned int);
 int tanh_OD(int, float, float);
+int tremolo(int, float, float);
+int low_pass(int sample);
 
 #endif /* EFFECTS_H_ */
--- Software/C5535 ezdsp/main.c
+++ Software/C5535 ezdsp/main.c
@@ -28,12 +28,15 @@ void main(void) {
 	while(1){
 		aic3204_read(&left_input, &right_input);
 		delay_sample(left_input);
+        left_output = left_input;
+        right_output = right_input;
 
-		//left_output = delay(left_input, SAMPLES_PER_SECOND/9);
-		left_output = flange(left_input, .5, 50);
-		//left_output = tanh_OD(left_input, 100, .2);
-		//left_output = left_input;
-		//right_output = right_input;
+		//left_output = delay(left_output, SAMPLES_PER_SECOND/2);
+		//left_output = flange(left_output, 1, 75);
+		//left_output = tanh_OD(left_output, 50, .2);
+		//left_output = tremolo(left_output, 60, 50);
+		//left_output = low_pass(left_output);
+
 
 		aic3204_write(left_output*.5, right_output*.3);
 	}
